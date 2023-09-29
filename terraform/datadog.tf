4743b3012a64ea63861a92c3b1d6cd468fd42e2d
Nathan Reese <github@sect.net>
2023-09-29 13:42:29 -0400

    Future fun time scaffold

---
+++ terraform/datadog.tf
@@ -0,0 +1,7 @@
+Nothing to see here yet.
+
+We are just leaving this here to remind future us that storing application specific terraform here in the application is a good practice. 
+
+We could include this in our pipeline to make sure app deployments also update our app specific infrastructure.
+
+Monitors specific to the app are a good candidate for such practices.
